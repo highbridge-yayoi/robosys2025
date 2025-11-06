81c99fdad3061282bbbe155728fd2929b6271e60
High Bridge <hainu738@gmail.com>
2025-11-06 12:07:37 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
 	echo ${1}行目が違うよ
@@ -8,6 +8,15 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus_stdin)
+++ seq 5
+++ ./plus_stdin
++ out=15
 [ "${out}" = 15 ] || ng "$LINENO"
++ '[' 15 = 15 ']'
+
 [ "${res}" = 0 ] && echo OK
++ '[' 0 = 0 ']'
++ echo OK
+OK
 exit $res
++ exit 0
